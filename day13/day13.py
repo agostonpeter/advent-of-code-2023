e9b25dd1c567a09a4f28876f1dd78abdb001fb72
agostonpeter <agoptr@protonmail.com>
2023-12-13 21:57:52 +0100

    day13 part 2

--- day13/day13.py
+++ day13/day13.py
@@ -38,37 +38,65 @@ def splitForMirroring(pattern, mirrorLine):
         return top, bottom[:lengthDiff]
     else:
         return top, bottom
-
+def printPattern(pattern):
+    print("---------")
+    for line in pattern:
+        print(line)
+    print("---------")
+    
 # print(patterns)
 # print(getRotated(patterns[0]))
-
-print("============== PART ONE ==============")
-partOneResult = 0
-# print(len(patterns)
-for pattern in patterns:
-    # print(partOneResult)
-    mirrorLineFound = False
+def findPatternResult(pattern, originalResult = 0):
+    # printPattern(pattern)
     for i, row in enumerate(pattern):
         if i==0:
             continue
         top, bottom = splitForMirroring(pattern, i)
-        if isMirrored(top, bottom):
-            mirrorLineFound = True
-            partOneResult += 100 * i
-            break
-    if mirrorLineFound:
-        continue
+        if isMirrored(top, bottom) and originalResult != 100 * i:
+            return 100 * i
 
     rotatedPattern = getRotated(pattern)
     for i, column in enumerate(rotatedPattern):
         if i==0:
             continue
         top, bottom = splitForMirroring(rotatedPattern, i)
-        if isMirrored(top, bottom):
-            partOneResult += i
-            mirrorLineFound = True
-            break
-    if not mirrorLineFound:
-        print("NOT FOUND!")
+        if isMirrored(top, bottom) and originalResult != i:
+            return  i
+    
+print("============== PART ONE ==============")
+partOneResult = 0
+# print(len(patterns)
+for pattern in patterns:
+    partOneResult += findPatternResult(pattern)
 
 print(partOneResult)
+
+print("============== PART TWO ==============")
+def swapSmudge(pattern, i, j):
+    if pattern[i][j] == "#":
+        pattern[i] = pattern[i][:j] + "." + pattern[i][j+1:]
+    else:
+        pattern[i] = pattern[i][:j] + "#" + pattern[i][j+1:]
+    return pattern
+
+partTwoResult = 0
+for pattern in patterns:
+    originalResult = findPatternResult(pattern)
+    foundNewResult = False
+    for i, row in enumerate(pattern):
+        for j, column in enumerate(row):
+            swapSmudge(pattern, i, j)
+            modifiedResult = findPatternResult(pattern, originalResult)
+            if modifiedResult is not None:
+                # print(f"Old result: {originalResult}, new result: {modifiedResult}")
+                partTwoResult += modifiedResult
+                foundNewResult = True
+                break
+            else:
+                swapSmudge(pattern, i, j)
+        if foundNewResult:
+            break
+    if not foundNewResult:
+        print("NO new result :( ")
+print(partTwoResult)
+            
