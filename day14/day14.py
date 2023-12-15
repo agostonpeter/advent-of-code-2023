3eba14dc483a1fef33987d63f4fcdf0ae6b27269
agostonpeter <agoptr@protonmail.com>
2023-12-15 01:10:07 +0100

    day14 part2

--- day14/day14.py
+++ day14/day14.py
@@ -13,7 +13,7 @@ file_path = "./day14/day14.txt"
 
 parsedInput = read_input(file_path)
 
-def getRotated(matrix):
+def getTransposed(matrix):
     rotatedMatrix = ([[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))])
     return [''.join(row) for row in rotatedMatrix]
 def printPattern(pattern):
@@ -21,20 +21,79 @@ def printPattern(pattern):
     for line in pattern:
         print(line)
     print("---------")
+def getRotated(input):
+    # clockwise
+    return [''.join(row)[::-1] for row in zip(*input)]
+
+def tiltPlatform(platform, orientation): # 0: north, 1: east
+    preRotation = (orientation + 4 - 1) % 4
+    # print(f"preRotation: {preRotation}")
+    rotatedPlatform = platform
+    for i in range(preRotation):
+        rotatedPlatform = getRotated(rotatedPlatform)
+    # print("rotated platform:")
+    # printPattern(rotatedPlatform)
+    newPlatform = []
+    for line in rotatedPlatform:
+        newLine = ""
+        for linePart in line.split('#'):
+            rocks = linePart.count('O')
+            newLine += 'O' * rocks
+            newLine += '.' * (len(linePart)-rocks)
+            newLine += '#'
+        newPlatform.append(newLine[:-1])
+    # print("new platf")
+    # printPattern(newPlatform)
+    # print(f"afterRotation: {4-preRotation}")
+
+    for i in range(4-preRotation):
+        newPlatform = getRotated(newPlatform)
+    # printPattern(newPlatform)
+    return newPlatform
+
+def getLoad(platform):
+    transposedPlatform = getTransposed(platform)
+    result = 0
+    for line in transposedPlatform:
+        rocks = [index+1 for index, char in enumerate(line[::-1]) if char == 'O']
+        result += sum(rocks)
+    return result
 
 print("============== PART ONE ==============")
-partOneResult = 0
+partOnePlatform = tiltPlatform(parsedInput, 0)
+print(getLoad(partOnePlatform))
 
-platform = getRotated(parsedInput)
+print("============== PART TWO ==============")
 
-for line in platform:
-    load = len(line)
-    for linePart in line.split('#'):
-        rocks = linePart.count('O')
-        nextLoad = load - len(linePart) - 1
-        for i in range(rocks):
-            partOneResult += load
-            load -=1
-        load = nextLoad
 
-print(partOneResult)
+
+platform = parsedInput
+# printPattern(platform)
+
+
+seenPlatforms = []
+cycle = 0
+goal = 1000000000
+# goal = 50
+repetitionFound = False
+while cycle < goal:
+    # print(cycle)
+    for tilt in range(4):
+        # printPattern(platform)
+        # print("tilt!")
+        platform = tiltPlatform(platform, tilt)
+        # printPattern(platform)
+    # print(f"cycle: {cycle}, load: {getLoad(platform)}")
+    if platform in seenPlatforms and not repetitionFound:
+        before = seenPlatforms.index(platform)
+        print(f"I've been here! Now: {cycle} before: {before}")
+        # printPattern(platform)
+        cycle = goal - ((goal - before) % (cycle - before))
+        print(f"cycle set to {cycle}")
+        cycle += 1
+        repetitionFound = True
+    else:
+        seenPlatforms.append(platform)
+        cycle += 1
+
+print(getLoad(platform))
